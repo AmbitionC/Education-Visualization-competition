7a3ab296cf29bcd10f1d9fd60e6da8c6504d6887
Chen Hao <823147833@qq.com>
2019-04-16 22:57:57 +0800

    update

--- Data-preprocess/1.School-Level/5.3in7.py
+++ Data-preprocess/1.School-Level/5.3in7.py
@@ -49,13 +49,29 @@ def statistic_score_info():
     # 统计各个学科的考试的数量
     subname_6 = ['语文', '数学', '英语', '音乐', '美术', '体育']
     data_merge_byStdID['count'] = 1
-    # subname_7 = ['物理', '化学', '生物', '政治', '历史', '地理', '技术']
-    # sub_students_choose = []
+    statistic_sub_name = []
+    statistic_sub_num = []
+    sum = 0
     statistic_sub_data = data_merge_byStdID.groupby(['mes_sub_name']).count().reset_index().sort_values(by='count', axis=0, ascending=False)
+    # 去除掉非7选3的学科
     for i in range(len(subname_6)):
         statistic_sub_data = statistic_sub_data.drop(statistic_sub_data[statistic_sub_data['mes_sub_name'] == subname_6[i]].index)
+    # 计算课程的总量
     for i in range(statistic_sub_data.shape[0]):
-        
-
+        sum += statistic_sub_data['count'].iloc[i]
+    # 提取数据
+    for i in range(statistic_sub_data.shape[0]):
+        print(statistic_sub_data['mes_sub_name'].iloc[i], '的数据量为', statistic_sub_data['count'].iloc[i])
+        statistic_sub_num.append(round(statistic_sub_data['count'].iloc[i] / sum * 100, 2))
+        statistic_sub_name.append(statistic_sub_data['mes_sub_name'].iloc[i])
+    print(statistic_sub_name)
+    print(statistic_sub_num)
 
 statistic_score_info()
+# ['生物', '政治', '物理', '化学', '地理', '历史', '技术']
+# [19.25, 18.623, 15.63, 15.53, 14.44, 13.82, 2.70]
+
+# 统计高二的各个学科组合的数据情况，按照组合的形式划分，总共有35种组合的方式
+# 按照学生的id进行groupby，首先按照考试类型进行划分，然后按照考试的科目进行划分，如果不是6个科目的话进行剔除
+# 统计这35种情况
+# def statistic_sub_combination():
