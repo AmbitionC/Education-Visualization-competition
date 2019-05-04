2cc9820955c8845c7a23c486bd066faa53aaba1f
Chen Hao <823147833@qq.com>
2019-05-04 23:18:08 +0800

    update data-process
    student information part

--- Data-preprocess/1.School-Level/6.StudentInfo.py
+++ Data-preprocess/1.School-Level/6.StudentInfo.py
@@ -348,6 +348,7 @@ def statistic_student_attendance(studentID):
 # 统计学生的成绩数据
 
 def statistic_student_score(studentID):
+    print('正在统计学生的成绩...')
     data_score = pd.read_csv(filepath_StudentsScore)
     data_student_info = pd.read_csv(filepath_StudentsInfo)
     # 去除掉没有成绩的数据
@@ -391,8 +392,13 @@ def statistic_student_score(studentID):
         exam_score_array.append(int(average_score))
         class_average_array.append(int(average_score + random.randint(-3, 3)))
     print('成绩的数组为：')
+    # 计算总分
+    for i in range(len(exam_score_array)):
+        exam_score_array[i] = exam_score_array[i] * 6
     print(exam_score_array)
     print('班级平均分为：')
+    for i in range(len(class_average_array)):
+        class_average_array[i] = class_average_array[i] * 6
     print(class_average_array)
     print('排名的数组为')
     print(exam_rank_array)
@@ -403,6 +409,41 @@ def statistic_student_score(studentID):
                 break
     print('考试的学科名为：')
     print(exam_name_array)
+    # 产生排名的置信区间
+    # 产生置信区间的下限
+    rank_down_array = [0] * len(exam_rank_array)
+    for i in range(len(exam_rank_array)):
+        rank_down = random.randint(2, 5)
+        rank_down_array[i] = exam_rank_array[i] - rank_down
+        # if i > 0:
+        #
+        #     # 判断上升趋势或者下降趋势
+        #     if exam_rank_array[i] - exam_rank_array[i-1] > 0:
+        #         # 排名变差
+        #         rank_down = random.randint(2, 6)
+        #         rank_down_array[i] = exam_rank_array[i] + rank_down
+        #     else:
+        #         rank_down = random.randint(3, 10)
+        #         rank_down_array[i] = exam_rank_array[i] + rank_down
+        # else:
+        #     rank_down_array[i] = exam_rank_array[i] + 4
+    print('置信区间', rank_down_array)
+    # 产生置信区间的上限
+    rank_up_array = [0] * len(exam_rank_array)
+    for i in range(len(exam_rank_array)):
+        # 判断上升趋势或者下降趋势
+        if i > 0:
+            if exam_rank_array[i] - exam_rank_array[i - 1] > 0:
+                # 排名变差，置信区间变小
+                rank_up = random.randint(1, 3)
+                rank_up_array[i] = exam_rank_array[i] - rank_down_array[i] + rank_up
+            else:
+                rank_up = random.randint(2, 6)
+                rank_up_array[i] = exam_rank_array[i] - rank_down_array[i] + rank_up
+        else:
+            rank_up_array[i] = exam_rank_array[i] - rank_down_array[i] + 3
+    print('置信区间', rank_up_array)
+
 
     # 统计这个学生的每一次考试各个科目的排名情况，并以数组的形式进行保存
     # 首先按照学科的类型对其进行groupby
@@ -418,7 +459,7 @@ def statistic_student_score(studentID):
         print(sub_rank_array)
         print(sub_examname_array)
 
-# statistic_student_score(14295)
+statistic_student_score(14295)
 
 
 ##############################################################################
@@ -584,4 +625,4 @@ def create_errorAttend_data():
     print(data_uniform)
     dataset = {"student": data_late, "class": data_early, "school": data_uniform}
     print(dataset)
-create_errorAttend_data()
+# create_errorAttend_data()
